8b6b44f4dc10b1d3a237f8beebc0a5f9a1d56f01
AlexeiKarpachBY <alexeikarpach@gmail.com>
2021-01-16 23:34:49 +0300

    Start

--- src/Cars/Cabriolet.java
+++ src/Cars/Cabriolet.java
@@ -2,4 +2,6 @@ package Cars;
 
 public class Cabriolet extends PassengerСar{
     //Для Саши
+    //Не забываем делать ветки!!!!!
+    //Делаем только комиты, пушим вечером!!!
 }
--- src/Cars/PassengerСar.java
+++ src/Cars/PassengerСar.java
@@ -2,4 +2,6 @@ package Cars;
 
 public class PassengerСar extends Car{
     //Для Андрея
+    //Не забываем делать ветки!!!!!
+    //Делаем только комиты, пушим вечером!!!
 }
