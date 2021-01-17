986f4ee8be3d1043701cc8e8e67b1a8d47029886
AlexeiKarpachBY <alexeikarpach@gmail.com>
2021-01-17 20:15:23 +0300

    Create getter, setter, and abstract method.

--- src/Cars/Car.java
+++ src/Cars/Car.java
@@ -1,6 +1,46 @@
 package Cars;
 
-public class Car {
+import java.util.Objects;
+
+public abstract class  Car {
     //Не забываем делать ветки!!!!!
     //Делаем только комиты, пушим вечером!!!
+
+     protected String car;
+
+    protected Car(String car) {
+        this.car = car;
+    }
+
+    public String getCar() {
+        return car;
+    }
+
+    public void setCar(String car) {
+        this.car = car;
+    }
+
+    public abstract void carType(String type);
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car1 = (Car) o;
+        return Objects.equals(car, car1.car);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(car);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "car='" + car + '\'' +
+                '}';
+    }
 }
