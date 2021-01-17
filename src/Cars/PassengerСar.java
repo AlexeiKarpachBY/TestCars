a8b54286c18f635beb6d7118104a2f636082cab2
vapbyblr <vapbyblr@gmail.com>
2021-01-17 18:28:52 +0300

    Andrei create new methods, constructors and fields in class PassengerСar

--- src/Cars/PassengerСar.java
+++ src/Cars/PassengerСar.java
@@ -1,7 +1,34 @@
 package Cars;
 
-public class PassengerСar extends Car{
-    //Для Андрея
-    //Не забываем делать ветки!!!!!
-    //Делаем только комиты, пушим вечером!!!
+public class PassengerСar extends Car {
+
+    protected int numberOfPassengerSeats;
+    protected boolean isPassengerInCar = false;
+
+    protected PassengerСar() {
+    }
+
+    public PassengerСar(int numberOfPassengerSeats) {
+        this.numberOfPassengerSeats = numberOfPassengerSeats;
+    }
+
+    protected void boardingPassengers(int numberOfPassenger) {
+        if (isPassengerInCar == false & numberOfPassenger <= numberOfPassengerSeats) {
+            isPassengerInCar = true;
+            System.out.println("Passenger in car");
+        }
+    }
+
+    protected void disembarkingPassengers() {
+        isPassengerInCar = false;
+        System.out.println("Passengers disembarked");
+    }
+
+    public int getNumberOfPassengerSeats() {
+        return numberOfPassengerSeats;
+    }
+
+    public void setNumberOfPassengerSeats(int numberOfPassengerSeats) {
+        this.numberOfPassengerSeats = numberOfPassengerSeats;
+    }
 }
